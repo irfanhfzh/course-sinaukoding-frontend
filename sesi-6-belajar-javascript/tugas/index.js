8d8eada78aded2a23f10d675e4f7d41bea3f0c84
irfanhfzh <58616380+irfanhfzh@users.noreply.github.com>
2022-08-15 19:27:28 +0700

    tugas sesi-6-belajar-javascript

--- sesi-6-belajar-javascript/tugas/index.js
+++ sesi-6-belajar-javascript/tugas/index.js
@@ -1,5 +1,5 @@
 // Tugas Sesi 6 - Belajar Javascript
-
+// Menampilkan Angkot No 1 - 10 beroperasi dengan baik. menggunakan while loop
 var nilaiAwal = 1;
 while (nilaiAwal <= 10) {
   console.log("Angkot No. " + nilaiAwal + " beroperasi dengan baik.");
